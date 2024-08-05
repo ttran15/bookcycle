8d159269cd46c1189342c3aab3649aa4b438b1fc
ttran15 <ttran15@lakeheadu.ca>
2024-08-05 05:37:12 -0400

    Edit Toast notification

--- app/src/main/java/com/example/bookcycle/HomeActivity.kt
+++ app/src/main/java/com/example/bookcycle/HomeActivity.kt
@@ -105,7 +105,7 @@ class HomeActivity : AppCompatActivity() {
     private fun loadFilesFromFirestore() {
         db.collection("files").addSnapshotListener { snapshots, e ->
             if (e != null) {
-                Toast.makeText(this, "Error loading files.", Toast.LENGTH_SHORT).show()
+//                Toast.makeText(this, "Error loading files.", Toast.LENGTH_SHORT).show()
                 return@addSnapshotListener
             }
 
